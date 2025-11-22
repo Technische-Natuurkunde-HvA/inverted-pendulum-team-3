ecbd757946a275019da0fd2d24175f7508ceef4d
JoranSt <joran.stavenuiter@hva.nl>
2025-11-22 20:03:02 +0100

    Added a final graph and made it so it writes to csv
    The file shows a final graph after the time of the measurement is up so you can look at it more closely. It also writes to a csv with the date and time so we can always find our measurements back

--- code/Python/dataV2.py
+++ code/Python/dataV2.py
@@ -2,7 +2,9 @@ import serial
 import time
 import re
 import matplotlib.pyplot as plt
-from collections import deque
+import pandas as pd
+from datetime import datetime
+
 # --- UPDATE THIS PORT ---
 PORT = "COM4"       # Windows example
 # PORT = "/dev/ttyACM0"   # Linux
@@ -14,9 +16,11 @@ duration = int(input("How long should the measurement be?:")) # choose a duratio
 ser = serial.Serial(PORT, BAUD, timeout=1)
 
 start = time.time() # start time
+now = datetime.now()
+filename = f"data_{now.strftime('%d-%m-%Y_%H-%M')}.csv"
 
 # Wait for Arduino reset (important!)
-time.sleep(2)
+#time.sleep(2)
 
 # makes sure the output can be split so we can use the values
 pattern = re.compile(
@@ -59,30 +63,53 @@ try:
             freq   = float(match.group(2))
             rpm    = float(match.group(3))
             angle  = float(match.group(4))
-            timelist.append(currenttime)
+            timelist.append(time.time() - start)
             anglelist.append(angle)
             rpmlist.append(rpm)
             outputlist.append(output)
             
-            anglegraph.set_xdata(range(len(timelist)))
+            anglegraph.set_xdata(list(timelist))
             anglegraph.set_ydata(list(anglelist))
 
-            outputgraph.set_xdata(range(len(timelist)))
+            outputgraph.set_xdata(list(timelist))
             outputgraph.set_ydata(list(outputlist))
 
-            rpmgraph.set_xdata(range(len(timelist)))
+            rpmgraph.set_xdata(list(timelist))
             rpmgraph.set_ydata(list(rpmlist))
 
-            currenttime+=0.5
             ax1.relim();  ax1.autoscale_view()
             ax2.relim();  ax2.autoscale_view()
             ax3.relim();  ax3.autoscale_view()
             plt.pause(0.01)
-
 except KeyboardInterrupt:
     print("Stopped.")
-    plt.close('all')
+    
 finally:
     ser.close()        
 
+
+data = {
+    "Time": timelist,
+    "Angle": anglelist,
+    "Output": outputlist,
+    "RPM": rpmlist
+}
+
+df = pd.DataFrame(data)
+df.to_csv(f'data/{filename}', index=False)
+
+
+plt.ioff()
+plt.close('all')
+
+
+fig2, (ax1f, ax2f, ax3f) = plt.subplots(3, 1, figsize=(8, 10))
+ax1f.plot(timelist, anglelist, label="Angle")
+ax2f.plot(timelist, outputlist, label="Output")
+ax3f.plot(timelist, rpmlist, label="RPM")
+ax1f.set_ylabel("Angle (deg)"); ax2f.set_ylabel("Output"); ax3f.set_ylabel("RPM")
+ax1f.set_xlabel("Time (s)");ax2f.set_xlabel("Time (s)");ax3f.set_xlabel("Time (s)")
+ax1f.legend(); ax2f.legend(); ax3f.legend()
+plt.tight_layout()
+plt.show()
 
