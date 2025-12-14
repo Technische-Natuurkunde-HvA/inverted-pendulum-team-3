38df91f4bedf5001cb076c6cbe3cccb4e7c8e842
JoranSt <joran.stavenuiter@hva.nl>
2025-12-14 14:34:06 +0100

    set the right file

--- code/Python/csvreader.py
+++ code/Python/csvreader.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("data\DataUpsideDownMeting.csv")
+df = pd.read_csv("data\DataUpsideDownFinal.csv")
 
 df.plot(x='Time', subplots=True, layout=(3, 1), figsize=(8, 10), title=['Angle', 'Output', 'RPM'])
 plt.tight_layout()  # adjusts spacing
